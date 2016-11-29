e30f1db1a0748c54e309dd6fc7c156146569cbcb
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-11-29 09:37:12 +0100

    Start gameplay + background + audio

---
+++ Game Design 2 v0.1/Assets/Scripts/Build.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Build : MonoBehaviour {
+
+	public GameObject wheel; //just wheel
+	public GameObject bottom; //just bottom
+	public GameObject bottomWheelBoth;
+	public GameObject bottomBar;
+	public GameObject bottomBothBar;
+	bool bottomWheelDone = false;
+	AudioSource audio;
+	AudioSource audio2;
+	public AudioClip inside; //audio clip for when an object is placed correctly
+	public AudioClip error;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		audio2 = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (bottomWheelBoth.activeInHierarchy == true) {
+			bottomWheelDone = true;
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "wheel") 
+		{
+			audio.PlayOneShot (inside, 0.5f);
+				wheel.SetActive (false);
+				bottomWheelBoth.SetActive (true);
+			
+		}
+		if (other.gameObject.name == "Bar1") {
+			if (bottomWheelDone == true) {
+
+			}
+		} else {
+			audio2.PlayOneShot (error);
+		}
+
+	}
+}
--- Game Design 2 v0.1/Assets/Scripts/CanvaScript.cs
+++ Game Design 2 v0.1/Assets/Scripts/CanvaScript.cs
@@ -10,18 +10,14 @@ public class CanvaScript : MonoBehaviour {
 	public GameObject ob3;
 	public GameObject ob4;
 	public GameObject ob5;
-	public GameObject ob6;
-	public GameObject ob7;
 	public GameObject ob8;
 	// Use this for initialization
 	void Start () {
 		ob1.GetComponent<BoxCollider2D>().enabled = false;
 		ob2.GetComponent<BoxCollider2D>().enabled = false;
-		ob3.GetComponent<BoxCollider2D>().enabled = false;
+		ob3.GetComponent<CircleCollider2D>().enabled = false;
 		ob4.GetComponent<BoxCollider2D>().enabled = false;
 		ob5.GetComponent<BoxCollider2D>().enabled = false;
-		ob6.GetComponent<BoxCollider2D>().enabled = false;
-		ob7.GetComponent<BoxCollider2D>().enabled = false;
 		ob8.GetComponent<BoxCollider2D>().enabled = false;
 		StartCoroutine(Canvas());
 	}
@@ -35,11 +31,9 @@ public class CanvaScript : MonoBehaviour {
 		canvas2.gameObject.SetActive (false);
 		ob1.GetComponent<BoxCollider2D>().enabled = true;
 		ob2.GetComponent<BoxCollider2D>().enabled = true;
-		ob3.GetComponent<BoxCollider2D>().enabled = true;
+		ob3.GetComponent<CircleCollider2D>().enabled = true;
 		ob4.GetComponent<BoxCollider2D>().enabled = true;
 		ob5.GetComponent<BoxCollider2D>().enabled = true;
-		ob6.GetComponent<BoxCollider2D>().enabled = true;
-		ob7.GetComponent<BoxCollider2D>().enabled = true;
 		ob8.GetComponent<BoxCollider2D>().enabled = true;
 	}
 }
