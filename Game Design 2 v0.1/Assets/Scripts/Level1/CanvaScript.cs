ba6d3118dc342484fae2f75aaf33c70192402899
lordalisat <b.wieringa31@gmail.com>
2016-12-04 16:18:02 +0100

    Highlighting + added script for easy level building

--- Game Design 2 v0.1/Assets/Scripts/Level1/CanvaScript.cs
+++ Game Design 2 v0.1/Assets/Scripts/Level1/CanvaScript.cs
@@ -1,24 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CanvaScript : MonoBehaviour {
 
 	public Transform canvas1;
 	public Transform canvas2;
-	public GameObject ob1;
-	public GameObject ob2;
-	public GameObject ob3;
-	public GameObject ob4;
-	public GameObject ob5;
-	public GameObject ob8;
+	private GameObject[] tileObjects;
 	// Use this for initialization
 	void Start () {
-		ob1.GetComponent<PolygonCollider2D>().enabled = false;
-		ob2.GetComponent<BoxCollider2D>().enabled = false;
-		ob3.GetComponent<CircleCollider2D>().enabled = false;
-		ob4.GetComponent<BoxCollider2D>().enabled = false;
-		ob5.GetComponent<PolygonCollider2D>().enabled = false;
-		ob8.GetComponent<PolygonCollider2D>().enabled = false;
+		tileObjects = GameObject.FindGameObjectsWithTag("Tile");
+		foreach (GameObject tileObject in tileObjects) {
+			tileObject.GetComponent<Collider2D>().enabled = false;
+		}
 		StartCoroutine(Canvas());
 	}
 	
@@ -29,11 +23,8 @@ public class CanvaScript : MonoBehaviour {
 		canvas2.gameObject.SetActive (true);
 		yield return new WaitForSeconds (1);
 		canvas2.gameObject.SetActive (false);
-		ob1.GetComponent<PolygonCollider2D>().enabled = true;
-		ob2.GetComponent<BoxCollider2D>().enabled = true;
-		ob3.GetComponent<CircleCollider2D>().enabled = true;
-		ob4.GetComponent<BoxCollider2D>().enabled = true;
-		ob5.GetComponent<PolygonCollider2D>().enabled = true;
-		ob8.GetComponent<PolygonCollider2D>().enabled = true;
+		foreach (GameObject tileObject in tileObjects) {
+			tileObject.GetComponent<Collider2D>().enabled = true;
+		}
 	}
 }
---
+++ Game Design 2 v0.1/Assets/Scripts/Tile.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour
+{
+
+	private Transform touchingTile;
+	private AudioSource audSource;
+	private Sprite startSprite;
+	public Sprite highlightSprite;
+	private SpriteRenderer renderer;
+	public string tag;
+
+	private void Awake()
+	{
+		audSource = gameObject.GetComponent<AudioSource>();
+		renderer = GetComponent<SpriteRenderer>();
+		startSprite = renderer.sprite;
+	}
+
+	private void OnMouseEnter()
+	{
+		if (!GameObject.FindWithTag("MainCamera").GetComponent<InputManager>().draggingItem) {
+			renderer.sprite = highlightSprite;
+		}
+	}
+
+	void OnMouseExit()
+	{
+		renderer.sprite = startSprite;
+	}
+
+	public void PickUp()
+	{
+		renderer.sprite = startSprite;
+		transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+		gameObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
+	}
+
+	public void Drop()
+	{
+		transform.localScale = new Vector3(1, 1, 1);
+		gameObject.GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+		Vector2 newPosition;
+		if (touchingTile == null) {
+			return;
+		}
+
+		var parentCell = touchingTile;
+		newPosition = parentCell.position;
+		transform.parent = parentCell;
+		StartCoroutine(SlotIntoPlace(transform.position, newPosition));
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == tag) {
+			if (other.transform.childCount == 0) {
+				touchingTile = other.transform;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == tag) {
+			touchingTile = null;
+		}
+	}
+
+	IEnumerator SlotIntoPlace(Vector2 startingPos, Vector2 endingPos)
+	{
+		float duration = 0.1f;
+		float elapsedTime = 0;
+		audSource.Play();
+		while (elapsedTime < duration) {
+			transform.position = Vector2.Lerp(startingPos, endingPos, elapsedTime / duration);
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		transform.position = endingPos;
+	}
+}
