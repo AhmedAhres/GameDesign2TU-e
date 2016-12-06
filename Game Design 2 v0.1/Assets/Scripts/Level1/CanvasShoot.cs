663ad1e67ccd3beea87cc56a1c5551c5677f8356
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-12-06 09:58:48 +0100

    Some stuff

---
+++ Game Design 2 v0.1/Assets/Scripts/Level1/CanvasShoot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CanvasShoot : MonoBehaviour {
+
+	public Transform canvas1;
+	//private GameObject[] tileObjects;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Canvas());
+	}
+	
+	// Update is called once per frame
+	IEnumerator Canvas () {
+		canvas1.gameObject.SetActive (true);
+		yield return new WaitForSeconds (2);
+		canvas1.gameObject.SetActive (false);
+
+	}
+}
---
+++ Game Design 2 v0.1/Assets/Scripts/Level1/Shoot.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject snowman1;
+	public GameObject snowman2;
+	public GameObject sphere;
+	int pressed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion rot = transform.rotation;
+
+		if (Input.GetKeyDown ("space")) {
+			if (pressed == 0) {
+				rot = Quaternion.Euler (0, 0, -28);
+				transform.rotation = rot;
+				//sphere.transform.localPosition.x += new Vector3 (0.1f, 0, 0);
+				StartCoroutine (Snowman ());
+				pressed = 1;
+			}
+		}
+
+			
+		
+	}
+	IEnumerator Snowman () {
+		yield return new WaitForSeconds (2);
+		snowman1.SetActive (false);
+		snowman2.SetActive (true);
+	}
+}
