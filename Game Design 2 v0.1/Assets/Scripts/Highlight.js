b7f022001078cbd612ad8b2d3e917cd6b4ecca4e
lordalisat <b.wieringa31@gmail.com>
2016-12-09 16:45:15 +0100

    Finished All levels besides advance to next level

--- Game Design 2 v0.1/Assets/Scripts/Highlight.js
+++
@@ -1,9 +0,0 @@
-﻿function OnMouseOver()
-{
-GetComponent.<Renderer>().material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
-}
-
-function OnMouseExit()
-{
-GetComponent.<Renderer>().material.shader = Shader.Find("Legacy Shaders/Transparent/Diffuse");
-}
