a3cae8153d64a0e530e58de48883a51abf4665fc
lordalisat <b.wieringa31@gmail.com>
2016-12-13 11:46:09 +0100

    Rocket Flight

--- Game Design 2 v0.1/Assets/Scripts/Level3/Fly.cs
+++ Game Design 2 v0.1/Assets/Scripts/Level3/Fly.cs
@@ -5,18 +5,30 @@ public class Fly : MonoBehaviour {
 
 	public Transform canv;
 	public GameObject rocket;
+	private Component[] engines;
+	private Vector3 startPosition;
 	float y;
 	float ratio;
 	// Use this for initialization
 	void Start () {
 		ratio = 0f;
+		engines = GetComponentsInChildren<ParticleSystem>();
+		startPosition = transform.localPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ratio += 0.01f;
-		if (Input.GetKey ("space") && canv.gameObject.activeInHierarchy == false) {
-			transform.localPosition += new Vector3 (0f, 0.05f*ratio, 0f);
+		if (Input.GetKey("space") && canv.gameObject.activeInHierarchy == false)
+			ratio += 0.01f;
+		else if (transform.localPosition.y >= startPosition.y && transform.localPosition.y <= 13)
+			ratio = Mathf.Max(ratio - 0.005f, -1.0f);
+		else
+			ratio = 0.0f;
+		transform.localPosition += new Vector3 (0f, 0.05f*ratio, 0f);
+		foreach (ParticleSystem engine in engines) {
+			var ps = engine.main;
+			ps.startSize = Mathf.Min(ratio, 1.0f);
+			ps.startLifetime = Mathf.Min(ratio*0.5f, 0.3f);
 		}
 	}
 }
--- Game Design 2 v0.1/Assets/Scripts/Level3/Fly2.cs
+++ Game Design 2 v0.1/Assets/Scripts/Level3/Fly2.cs
@@ -5,18 +5,19 @@ public class Fly2 : MonoBehaviour {
 
 	public GameObject rocket;
 	public Transform canv;
+	public GameObject rocket1;
 	float y;
 	float ratio;
 	// Use this for initialization
 	void Start () {
-		ratio = 0f;
+		ratio = 2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		ratio += 0.01f;
-		if (Input.GetKey ("space") && transform.localPosition.y>2 && canv.gameObject.activeInHierarchy == false) {
+		if (transform.localPosition.y <= 2)
+			ratio = 0f;
+		if (rocket1.transform.localPosition.y > 13)
 			transform.localPosition += new Vector3 (0f, -0.05f*ratio, 0f);
-		}
 	}
 }
