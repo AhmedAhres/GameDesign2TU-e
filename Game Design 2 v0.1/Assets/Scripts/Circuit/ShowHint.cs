57e036f3074b0d6611cbf0026ccae0d038c71389
xhilusius <k.bouwmeester@hotmail.com>
2016-12-20 12:08:01 +0100

    ShowHints

--- Game Design 2 v0.1/Assets/Scripts/Circuit/ShowHint.cs
+++ Game Design 2 v0.1/Assets/Scripts/Circuit/ShowHint.cs
@@ -9,6 +9,15 @@ public class ShowHint : MonoBehaviour {
 	private bool hintsAreOn;
 	// Use this for initialization
 	void Start () {
+		lights.SetActive (false);
+		hints.SetActive (true);
+		hintsAreOn = true;
+		Invoke ("initializeHints", 8.0f);
+	}
+
+	void initializeHints(){
+		lights.SetActive (true);
+		hints.SetActive (false);
 		hintsAreOn = false;
 	}
 	
