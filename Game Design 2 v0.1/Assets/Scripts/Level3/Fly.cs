ec07360c28944fc0d5d5560dfabaf986896d64d2
lordalisat <b.wieringa31@gmail.com>
2016-12-20 11:22:56 +0100

    RobotPath

--- Game Design 2 v0.1/Assets/Scripts/Level3/Fly.cs
+++ Game Design 2 v0.1/Assets/Scripts/Level3/Fly.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Fly : MonoBehaviour {
+public class Fly : MonoBehaviour
+{
 
 	public Transform canv;
 	public GameObject rocket;
@@ -12,42 +13,80 @@ public class Fly : MonoBehaviour {
 	bool playAudio = false;
 	float y;
 	float ratio;
+	public bool achievement;
+	private bool robotAnimation, active;
+	private GameObject robot;
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		ratio = 0f;
 		engines = GetComponentsInChildren<ParticleSystem>();
 		startPosition = transform.localPosition;
-		launch = GetComponent<AudioSource> ();
+		launch = GetComponent<AudioSource>();
+		robot = GameObject.FindGameObjectWithTag("Robot");
+		robot.SetActive(false);
 	}
 
-	void playMusic(){
-		launch.PlayOneShot (launcher, 0.7f);
+	void playMusic()
+	{
+		launch.PlayOneShot(launcher, 0.7f);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-		if (!playAudio) {
-			if (Input.GetKey ("space") && canv.gameObject.activeInHierarchy == false) {
-				launch.PlayOneShot (launcher, 0.7f);
-				ratio += 0.01f;
-				playAudio = true;
+	void Update()
+	{
+		if (!robotAnimation || !achievement) {
+			if (transform.localPosition.y > startPosition.y) {
+				if (Input.GetKey("space") && canv.gameObject.activeInHierarchy == false) {
+					if (playAudio) {
+						ratio += 0.01f;
+					} else {
+						launch.PlayOneShot(launcher, 0.7f);
+						playAudio = true;
+						ratio += 0.01f;
+					}
+				} else if (transform.localPosition.y <= 13) {
+					ratio = Mathf.Max(ratio - 0.005f, -1.0f);
+				}
+			} else {
+				if (ratio < -0.7f) {
+					robotAnimation = true;
+				}
+				if (ratio < 0.0f) {
+					ratio = 0.0f;
+				}
+				if (Input.GetKey("space") && canv.gameObject.activeInHierarchy == false) {
+					if (playAudio) {
+						ratio += 0.01f;
+					} else {
+						launch.PlayOneShot(launcher, 0.7f);
+						playAudio = true;
+						ratio += 0.01f;
+					}
+				}
 			}
+		} else {
+			if (!active)
+			StartCoroutine(RobotActivate());
+			active = true;
 		}
-		if (playAudio) {
-			if (Input.GetKey ("space") && canv.gameObject.activeInHierarchy == false) {
-				ratio += 0.01f;
-			}
-		} else if (transform.localPosition.y >= startPosition.y && transform.localPosition.y <= 13)
-			ratio = Mathf.Max (ratio - 0.005f, -1.0f);
-		else 
-			ratio = 0.0f;
 		
-		transform.localPosition += new Vector3 (0f, 0.05f*ratio, 0f);
+		if (ratio <= 0) {
+			launch.Stop();
+			playAudio = false;
+		}
+
+		transform.localPosition += new Vector3(0f, 0.05f * ratio, 0f);
 		foreach (ParticleSystem engine in engines) {
 			var ps = engine.main;
 			ps.startSize = Mathf.Min(ratio, 1.0f);
-			ps.startLifetime = Mathf.Min(ratio*0.5f, 0.3f);
+			ps.startLifetime = Mathf.Min(ratio * 0.5f, 0.3f);
 		}
+	}
+
+	IEnumerator RobotActivate() {
+		yield return new WaitForSeconds(1);
+		robot.SetActive(true);
+		robot.SendMessage("Move");
 	}
 }
---
+++ Game Design 2 v0.1/Assets/Scripts/Level3/Path.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+	// Use this for initialization
+	void Move () {
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("RobotPath"), "time", 5, "easetype", iTween.EaseType.easeInOutCubic));
+	}
+
+}
