e8f3a206efa08ec3e824181f8035df5458acde5d
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2016-12-20 11:18:15 +0100

    bug

--- Game Design 2 v0.1/Assets/Scripts/Level1/MoveSample1.cs
+++ Game Design 2 v0.1/Assets/Scripts/Level1/MoveSample1.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MoveSample1 : MonoBehaviour
 {	
 	void Start(){
-		iTween.MoveTo(gameObject, iTween.Hash("x", 5.2f, "y", 0f, "time", 0.5));
+		iTween.MoveTo(gameObject, iTween.Hash("x", 5.0f, "y", 0f, "time", 0.5));
 	}
 }
 
