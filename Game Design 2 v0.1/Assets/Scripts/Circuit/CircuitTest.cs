0b0252c460d576985c3e4d5c12ed57190608b1e7
lordalisat <b.wieringa31@gmail.com>
2017-01-09 10:39:33 +0100

    Circuit

--- Game Design 2 v0.1/Assets/Scripts/Circuit/CircuitTest.cs
+++ Game Design 2 v0.1/Assets/Scripts/Circuit/CircuitTest.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircuitTest : MonoBehaviour {
+public class CircuitTest : MonoBehaviour
+{
 	public GameObject[] connectorArray;
 	public GameObject[] cableArray;
 	public GameObject lastClicked;
@@ -10,26 +11,24 @@ public class CircuitTest : MonoBehaviour {
 	public string possibleName;
 	public string anotherName;
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		
 	}
-	
+
 	void Update()
 	{
-		if( Input.GetMouseButtonDown(0) )
-		{
-			Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
-			if( Physics.Raycast( ray, out hit, 100 ) )
-			{
-				Debug.Log( hit.transform.gameObject.name );
+			if (Physics.Raycast(ray, out hit, 100)) {
+				Debug.Log(hit.transform.gameObject.name);
 				currentlyClicked = hit.transform.gameObject;
 
-
 				for (int i = 0; i < cableArray.Length; i++) {
-					if (cableArray [i] == currentlyClicked) {
-						cableArray [i].SetActive (false);
+					if (cableArray[i] == currentlyClicked) {
+						cableArray[i].SetActive(false);
 						lastClicked = null;
 						currentlyClicked = null;
 						possibleName = null;
@@ -38,33 +37,32 @@ public class CircuitTest : MonoBehaviour {
 					}
 				}
 
-
-
-				if (currentlyClicked.name == "Background" || currentlyClicked == lastClicked || currentlyClicked.name == "Help") {
-					currentlyClicked = null;
-
-				
-				} else if (lastClicked == null) {
-					lastClicked = currentlyClicked;
-					currentlyClicked = null;
-				} else if (lastClicked != currentlyClicked) {
-					possibleName = lastClicked.name + currentlyClicked.name;
-					anotherName = currentlyClicked.name + lastClicked.name;
-					int refNumber;
-					for (int i = 0; i < cableArray.Length; i++) {
-						if (cableArray [i].name == possibleName || cableArray [i].name == anotherName) {
-							cableArray [i].SetActive (true);
-							lastClicked = null;
-							currentlyClicked = null;
-							possibleName = null;
-							anotherName = null;
-							break;
+				if (currentlyClicked != null){
+					if (currentlyClicked.name == "Background" || currentlyClicked == lastClicked || currentlyClicked.name == "Help") {
+						currentlyClicked = null;
+						lastClicked = null;
+					} else if (lastClicked == null) {
+						lastClicked = currentlyClicked;
+						currentlyClicked = null;
+					} else if (lastClicked != currentlyClicked) {
+						possibleName = lastClicked.name + currentlyClicked.name;
+						anotherName = currentlyClicked.name + lastClicked.name;
+						int refNumber;
+						for (int i = 0; i < cableArray.Length; i++) {
+							if (cableArray[i].name == possibleName || cableArray[i].name == anotherName) {
+								cableArray[i].SetActive(true);
+								possibleName = null;
+								anotherName = null;
+								break;
+							}
 						}
+						lastClicked = currentlyClicked;
+						currentlyClicked = null;
 					}
 				}
 			}
 		}
-		if (Input.GetMouseButtonDown (1)) {
+		if (Input.GetMouseButtonDown(1)) {
 			currentlyClicked = null;
 			lastClicked = null;
 		}
